b19632e1e1afc80244880ab59e81b2806876ff07
MiCHiLU <djmchl@gmail.com>
2013-12-31 14:16:11 +0700

    2013/12/30

--- project/amazon.py
+++ project/amazon.py
@@ -1,6 +1,9 @@
 #!/bin/usr/env python
 # -*- coding: utf-8 -*-
 
+import logging
+import urllib2
+
 from bottle import (
     default_app,
     route,
@@ -10,14 +13,26 @@ from bottle import (
     request,
 )
 
+from amazonproduct import API, errors
+
+api = API(locale='jp')
+
 @get("/")
 def amazon_get():
-    return template("amazon")
+    return template("amazon", keywords="", results=[])
 
 @post("/")
 def amazon_post():
     keywords = request.forms.get('keywords')
-    return template("amazon", keywords=keywords)
+    results = list()
+    if keywords:
+        try:
+            results = api.item_search('Books', Title=keywords)
+        except errors.AWSError as e:
+            logging.error(u"{0}: {1}".format(e.__class__.__name__, e))
+        except urllib2.URLError as e:
+            logging.error("URLError: {0}".format(e))
+    return template("amazon", keywords=keywords, results=results)
 
 app = default_app()
 
